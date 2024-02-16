21e0fe2eed3dcc671939ed6d0d2116fc2c6f8a9e
Devp-sriram <sriramraman100@gmail.com>
2024-02-16 21:11:18 +0530

    launch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+
 function App() {
+  const [user, setUser] = useState(null);
+  const [slideDown, setSlideDown] = useState(true);
+
+
+  useEffect(() => {
+    fetch('https://randomuser.me/api/?page=1&results=1&seed=abc')
+      .then((response) => response.json())
+      .then((data) => setUser(data.results[0]));
+  }, []);
+  
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setSlideDown(false);
+    }, 0); // set the delay to 0 to make the animation start immediately
+
+    return () => {
+      clearTimeout(timer);
+    }
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className={`App ${slideDown ? 'slide-down' : ''}`}>
+      {user ? (
+        <div className={`card ${slideDown ? 'card-slide-down' : ''}`}>
+            <img src={user.picture.large} alt={`${user.name.first} ${user.name.last}`} />
+            <div className="text">
+              <h2>
+                {user.name.first} {user.name.last}
+              </h2>
+              <p>{user.email}</p>
+              <p>{user.location.city}, {user.location.country}</p>
+            </div>
+          </div>
+        ) : (
+          <p class="loading">loading....</p>
+        )}
+      </div>
   );
 }
 
